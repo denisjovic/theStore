2b5aefdbbd3c4f79bd7c0bdaf977d4038b865b5f
denis.jovic <denis.jovic@indysoft.com>
2023-11-20 09:31:41 -0500

    Add axios agent & toast for handling errors of FE side

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,5 +1,64 @@
-import { Typography } from "@mui/material";
+import { Typography, Container, ButtonGroup, Button } from "@mui/material";
+import agent from "../../api/agent";
 
 export default function AboutPage() {
-  return <Typography variant="h2">About Page</Typography>;
+  return (
+    <Container>
+      <Typography gutterBottom variant="h2">
+        Testing Errors
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((error) =>
+              console.error(error)
+            )
+          }
+        >
+          Test 400
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get401Error().catch((error) =>
+              console.error(error)
+            )
+          }
+        >
+          Test 401
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get404Error().catch((error) =>
+              console.error(error)
+            )
+          }
+        >
+          Test 404
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get500Error().catch((error) =>
+              console.error(error)
+            )
+          }
+        >
+          Test 500
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.getValidationError().catch((error) =>
+              console.error(error)
+            )
+          }
+        >
+          Test Validation
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
 }
